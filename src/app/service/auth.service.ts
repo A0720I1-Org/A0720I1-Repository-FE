2df59f7719eef3b091dc525dea93132209f90a7c
quocdon <quocdon2991@gmail.com>
2021-07-07 16:35:51 +0700

    fix login httpOptions

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
+import {TokenStorageService} from "./token-storage.service";
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,11 @@ export class AuthService {
   AUTH_API = environment.apiBaseUrl
 
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) {
     this.httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ` + this.tokenStorage.getToken()
       }),
       'Access-Control-Allow-Origin': 'http://localhost:4200',
       'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
