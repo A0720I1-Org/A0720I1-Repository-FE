22cea39961bcaf309c06dfda5bb576188be141bd
quocdon <quocdon2991@gmail.com>
2021-07-11 15:16:41 +0700

    add class_v2

--- src/app/class-student/class-student.module.ts
+++ src/app/class-student/class-student.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ClassStudentRoutingModule } from './class-student-routing.module';
 import { CreateClassStudentComponent } from './create-class-student/create-class-student.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { CreateStudentComponent } from './create-student/create-student.component';
 
 
 @NgModule({
-  declarations: [CreateClassStudentComponent],
+  declarations: [CreateClassStudentComponent, CreateStudentComponent],
   imports: [
     CommonModule,
-    ClassStudentRoutingModule
+    ClassStudentRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class ClassStudentModule { }
--- src/app/class-student/create-class-student/create-class-student.component.ts
+++ src/app/class-student/create-class-student/create-class-student.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {TeacherService} from "../../service/teacher.service";
+import {ToastrService} from "ngx-toastr";
+import {SchoolYearService} from "../../service/school-year.service";
+import {SchoolYear} from "../../dto/schedule/SchoolYear";
+import {ClassCreate} from "../../dto/student-class/ClassCreate";
+import {ClassStudentService} from "../../service/class-student.service";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-create-class-student',
@@ -6,10 +15,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-class-student.component.scss']
 })
 export class CreateClassStudentComponent implements OnInit {
+  classForm: FormGroup;
+  validationMessage = {
+    'schoolYear': [
+      {type: 'required', message: 'Năm học không được để trống'},
 
-  constructor() { }
+    ],
+    'className': [
+      {type: 'required', message: 'Tên lớp không được để trống'},
+      {type: 'pattern', message: 'Tên lớp không đúng định dạng (Ví dụ: 1A, 1A5, 1/1)'},
+    ]
+  }
+  yearList: SchoolYear[] = null;
+  newClass: ClassCreate;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private teacherService: TeacherService,
+    private toastrService: ToastrService,
+    private schoolYearService: SchoolYearService,
+    private classStudentService: ClassStudentService,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.getYearList();
+    this.initForm();
   }
 
+  initForm(){
+    this.classForm = this.formBuilder.group({
+      schoolYear: this.formBuilder.control('', [
+        Validators.required,
+      ]),
+      className: this.formBuilder.control('', [
+        Validators.required,
+        Validators.pattern(/^1[A-Z][0-9]{0,2}$|^1\/[1-9][0-9]?$/)
+      ])
+    })
+  }
+
+
+  getYearList() {
+    this.schoolYearService.getAllSchoolYear().subscribe(
+      (data) => {
+        this.yearList = data;
+      }
+    )
+  }
+
+  onSubmit() {
+    this.newClass = new ClassCreate(this.classForm.get('schoolYear').value, 1, this.classForm.get('className').value);
+    this.classStudentService.createClass(this.newClass).subscribe(
+      (data) => {
+        this.toastrService.success(
+          "Thêm mới lớp thành công",
+          "Thông báo",
+          {timeOut: 3000, extendedTimeOut: 1500}
+        )
+        this.dialog.closeAll();
+      },
+      err => {
+        this.toastrService.error(
+          err.error.message,
+          "Có lỗi xảy ra",
+          {timeOut: 3000, extendedTimeOut: 1500}
+        )
+      }
+    )
+  }
+
+
+  closeDialog() {
+    this.dialog.closeAll();
+  }
 }
